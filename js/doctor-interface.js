0fadf123587fbc4589cbc532d0b2d0377bb1031e
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-09-15 12:04:48 -0700

    retrieve the names of doctors based on sympotom

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,17 @@
+var apiKey = require('./../.env').apiKey;
+import {Doctor} from './../js/doctorLookup.js';
 $(document).ready(function(){
-  $('#').submit(function(event){
+  let displayDoctors = function(doctors) {
+    console.log(doctors);
+    doctors.data.forEach(function(doctor){
+      console.log(doctor.profile.first_name);
+    });
+  };
+  $('#docSearch').submit(function(event){
     event.preventDefault();
+    let symptom = $('#symptom').val();
+    let doctorIndex = new Doctor(symptom);
 
+    doctorIndex.getDoctors(displayDoctors);
   });
 });
--- js/doctorLookup.js
+++ js/doctorLookup.js
@@ -1,1 +1,18 @@
-
+var apiKey = require('./../.env').apiKey;
+export class Doctor{
+  constuctor(symptom, doctorName){
+    this.symptom = symptom;
+    this.doctorName = doctorName;
+  }
+  getDoctors(displayDoctors){
+    let results;
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=or-portland&user_location=45.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey;
+    $.get(url)
+      .then((results) =>{
+        displayDoctors(results);
+      })
+      .fail(() =>{
+        console.log('something went wrong in getDoctors fucntion');
+      });
+  }
+}
