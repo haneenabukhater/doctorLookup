8dd6d813d95579c6e57c9f0d63fdb3ea6e06a288
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-09-15 12:37:16 -0700

    add functionality to look up doctor names by symptom

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,17 +1,23 @@
 var apiKey = require('./../.env').apiKey;
 import {Doctor} from './../js/doctorLookup.js';
+
 $(document).ready(function(){
+
   let displayDoctors = function(doctors) {
     console.log(doctors);
     doctors.data.forEach(function(doctor){
       console.log(doctor.profile.first_name);
+      $('ul#docList').append(`<li>${doctor.profile.first_name}`);
     });
+
   };
+
   $('#docSearch').submit(function(event){
     event.preventDefault();
-    let symptom = $('#symptom').val();
-    let doctorIndex = new Doctor(symptom);
+    let issue = $('#symptom').val();
 
-    doctorIndex.getDoctors(displayDoctors);
+    let doctorIndex = new Doctor(issue);
+
+    doctorIndex.getDoctors(issue,displayDoctors);
   });
 });
--- js/doctorLookup.js
+++ js/doctorLookup.js
@@ -1,12 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 export class Doctor{
-  constuctor(symptom, doctorName){
+  constuctor(symptom){
     this.symptom = symptom;
-    this.doctorName = doctorName;
+    // this.doctorName = doctorName;
   }
-  getDoctors(displayDoctors){
+  getDoctors(symptom, displayDoctors){
     let results;
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=headache&location=or-portland&user_location=45.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey;
+    console.log('your symptom is: ' + symptom);
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=45.773%2C-122.413&skip=0&limit=10&user_key=`+ apiKey;
     $.get(url)
       .then((results) =>{
         displayDoctors(results);
