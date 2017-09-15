a67b3906b3d17038009e8493d4e1a40aad0039e9
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-09-15 13:13:16 -0700

    added some css and ability to pull address and phone num

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,20 +4,32 @@ import {Doctor} from './../js/doctorLookup.js';
 $(document).ready(function(){
 
   let displayDoctors = function(doctors) {
-    console.log(doctors);
-    doctors.data.forEach(function(doctor){
-      console.log(doctor.profile.first_name);
-      $('ul#docList').append(`<li>${doctor.profile.first_name}`);
+    doctors.forEach(function(doctor){
+      console.log("display dr phone: "+doctor.phone);
+      $('ul#docList').append(`<li>Doctor Name: ${doctor.first_name} ${doctor.last_name}`);
     });
 
   };
 
   $('#docSearch').submit(function(event){
     event.preventDefault();
+    $('#docList li').remove();
     let issue = $('#symptom').val();
 
     let doctorIndex = new Doctor(issue);
 
     doctorIndex.getDoctors(issue,displayDoctors);
+    $('#response').show();
   });
 });
+
+
+
+// let displayDoctors = function(doctors) {
+//   console.log(doctors);
+//   doctors.data.forEach(function(doctor){
+//     console.log(doctor.profile.first_name);
+//     $('ul#docList').append(`<li>${doctor.profile.first_name}`);
+//   });
+//
+// };
--- js/doctorLookup.js
+++ js/doctorLookup.js
@@ -4,16 +4,46 @@ export class Doctor{
     this.symptom = symptom;
     // this.doctorName = doctorName;
   }
+  filterDocData(resultDocs){
+    let docArray = [];
+    resultDocs.data.forEach(function(doctor){
+      docArray.push({
+        "first_name": doctor.profile.first_name,
+        "last_name": doctor.profile.last_name,
+        "address": doctor.practices[0].visit_address.street,
+        "phone": doctor.practices[0].phones[0].number
+      });
+    });
+    return docArray;
+  }
   getDoctors(symptom, displayDoctors){
     let results;
+    let docResults;
     console.log('your symptom is: ' + symptom);
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=45.773%2C-122.413&skip=0&limit=10&user_key=`+ apiKey;
     $.get(url)
       .then((results) =>{
-        displayDoctors(results);
+        docResults = this.filterDocData(results);
+        displayDoctors(docResults);
       })
       .fail(() =>{
         console.log('something went wrong in getDoctors fucntion');
       });
   }
 }
+
+
+
+
+// getDoctors(symptom, displayDoctors){
+//   let results;
+//   console.log('your symptom is: ' + symptom);
+//   let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&user_location=45.773%2C-122.413&skip=0&limit=10&user_key=`+ apiKey;
+//   $.get(url)
+//     .then((results) =>{
+//       displayDoctors(results);
+//     })
+//     .fail(() =>{
+//       console.log('something went wrong in getDoctors fucntion');
+//     });
+// }
